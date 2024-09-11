741a15ec022adb400ca93c95e8683e4962e27ba0
Rodrigo <rodrigoyujik@gmail.com>
2024-09-11 09:34:54 -0400

    First commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NativeBaseProvider, Box, Text, Center } from 'native-base';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} bg="white">
+        <Box>
+          <Text fontSize="2xl" color="primary.500" fontWeight="bold" fontFamily="Roboto">
+            Olá, nosso app está configurado com Native Base!
+          </Text>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
+  );
+}
